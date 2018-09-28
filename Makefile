4168633fd97c26991302d67abeab282b87d4cb01
Anders Johansson <anjohan@uio.no>
2018-09-28 18:08:09 +0200

    lasso fungerer ikke, konvergerer enten på ett steg eller ikke i det hele tatt, avhengig av lambda

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 all: report.pdf
-	mkdir -p build && cd build && cmake .. && make
+	mkdir -p build && cd build && cmake -DCMAKE_BUILD_TYPE=Debug .. && make
 
 deps = sources.bib figs/franke.pdf
 
--- programs/test.f90
+++ programs/test.f90
@@ -1,6 +1,6 @@
 program test
     use iso_fortran_env, only: dp => real64
-    use mod_ridge2d
+    use mod_lasso2d
     implicit none
 
     class(polyfitter2d), allocatable :: pf
@@ -14,7 +14,7 @@ program test
 
     y = 1 + 2*x(:,1) + 3*x(:,1)**2 + 3.5*x(:,2) + 4*x(:,1)*x(:,2) + 5*x(:,2)**2
 
-    pf = ridge2d(d, 0.1_dp)
+    pf = lasso2d(d, 0.3_dp)
 
     call pf%fit(x, y)
 
