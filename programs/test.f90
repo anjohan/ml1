66600d059dc1c84c49841435984f687cd1e29663
Anders Johansson <anjohan@uio.no>
2018-10-01 11:48:54 +0200

    Generalised everything with basis functions & whole shebang, seems to work

--- programs/test.f90
+++ programs/test.f90
@@ -1,34 +1,41 @@
 program test
     use iso_fortran_env, only: dp => real64
-    use mod_ridge2d
-    use mod_lasso2d
+    use mod_polynomials2d
+    use mod_ols
+    use mod_ridge
+    use mod_lasso
     use mod_bootstrap
     implicit none
 
-    class(polyfitter2d), allocatable :: pf
+    class(regressor), allocatable :: fitter
     class(bootstrapper), allocatable :: bs
+    class(polynomial2d), allocatable :: basis(:)
     integer :: d = 2, N = 10000
     real(dp), allocatable :: x(:,:), y(:), y_pred(:)
     real(dp) :: mse, r2
 
     allocate(x(N,2), y(N), y_pred(N))
 
+    call create_basis(basis,d)
+
     call random_number(x)
 
     y = 1 - 2*x(:,1) + 0*x(:,1)**2 + 3.5*x(:,2) + 4*x(:,1)*x(:,2) + 5*x(:,2)**2
 
-    pf = ridge2d(d, 1.0_dp)
+    ! fitter = ols(basis=basis)
+    ! fitter = ridge(basis=basis, lambda=1.0_dp)
+    fitter = lasso(basis=basis, lambda=1.5_dp)
 
-    call pf%fit(x, y)
+    call fitter%fit(x, y)
 
-    write(*,*) pf%beta
+    write(*,*) fitter%beta
 
-    call pf%predict(x, y_pred, y, mse, r2)
+    call fitter%predict(x, y_pred, y, mse, r2)
     ! write(*,*) y
     ! write(*,*) y_pred
     write(*,*) mse, r2
 
-    bs = bootstrapper(pf)
+    bs = bootstrapper(fitter)
     call bs%bootstrap(x, y, 1000)
     write(*,*) sum(bs%betas, dim=2)/size(bs%betas, dim=2)
 end program
