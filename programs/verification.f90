ff9587fe108930a1fedba4a118464afa032a4f88
Anders Johansson <anjohan@uio.no>
2018-10-03 10:35:56 +0200

    memory error

--- programs/verification.f90
+++ programs/verification.f90
@@ -13,10 +13,10 @@ program verification
     class(bootstrapper), allocatable :: bs
     class(regressor), allocatable :: fitter
 
-    integer :: d, N, num_bootstraps, i
-    real(dp) :: test_fraction, lambda, sigma
+    integer :: d, N, num_bootstraps, i, u_bias_variance, u_y, u_mse_r2
+    real(dp) :: test_fraction, lambda, sigma, mse, r2
 
-    real(dp), allocatable :: x(:,:), y(:)
+    real(dp), allocatable :: x(:,:), y(:), y_prediction(:)
     d = 5
     N = 1000000
     sigma = 0.1
@@ -28,10 +28,24 @@ program verification
                regressor_container(ridge(lambda, basis)), &
                regressor_container(lasso(lambda, basis))]
 
+    open(newunit=u_bias_variance, file="data/verification_bias_variance.dat", &
+         status = "replace")
+    write(u_bias_variance, "(a)") "Method MSE Bias Variance"
+    open(newunit=u_mse_r2, file="data/verification_mse_r2.dat", status="replace")
+    write(u_mse_r2, "(a)") "Method MSE R2"
+
+    allocate(y_prediction(N))
+
     do i = 1, 3
         fitter = fitters(i)%element
         x = random_meshgrid(nint(sqrt(1.0d0*N)), "data/verification_" // fitter%method)
         y = franke(x)
         call add_noise(y, sigma)
+
+        call fitter%fit(x, y)
+        call fitter%predict(x, y_prediction, y, mse, r2)
+        write(u_mse_r2, "(a,x,f0.4,x,f0.3)") fitter%method, mse, r2
     end do
+    close(u_mse_r2)
+    close(u_bias_variance)
 end program
